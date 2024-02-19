5eb220e8d56d8742931c85ead05668eb457471d6
Mohamad Lakkis <mohamad.allakkis@gmail.com>
2024-02-19 10:37:41 +0200

    Update 231. Power of Two.py

--- LeetCode/231. Power of Two.py
+++ LeetCode/231. Power of Two.py
@@ -3,3 +3,8 @@ import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return n>0 and (n&(n-1))==0
+    
+    '''
+    def isPowerOfTwo(self,n:int)->bool:
+        return n>0 and (1<<30)%n==0
+    '''
