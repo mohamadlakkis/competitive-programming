16979871dd8faea20905a05ec824f61238198c11
Mohamad Lakkis <mohamad.allakkis@gmail.com>
2024-04-04 16:28:22 +0300

    Create 1614. Maximum Nesting Depth of the Parentheses.py

---
+++ LeetCode/1614. Maximum Nesting Depth of the Parentheses.py
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count = 0;
+        int max_num = 0;
+        for (char c : s) {
+            if (c == '(') {
+                count++;
+                if (max_num < count)
+                    max_num = count;
+            } else if (c == ')') {
+                count--;
+            }
+        }
+        return max_num;
+    }
+};
